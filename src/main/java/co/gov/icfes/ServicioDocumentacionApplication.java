0a71d93a01294c3a71a265d1f36d18becaedb0de
farid ureche <faridureche@javeriana.edu.co>
2020-07-28 17:26:17 -0500

    Configuracion config-server

--- src/main/java/co/gov/icfes/ServicioDocumentacionApplication.java
+++ src/main/java/co/gov/icfes/ServicioDocumentacionApplication.java
@@ -2,6 +2,7 @@ package co.gov.icfes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -9,6 +10,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableSwagger2
 @EnableScheduling
+@EnableEurekaClient
 public class ServicioDocumentacionApplication {
 
 	public static void main(String[] args) {

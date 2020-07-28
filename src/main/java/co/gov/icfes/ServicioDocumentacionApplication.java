7f09252b63090c0d2820ba2df86dfca73fe6090b
elturcofarid <elturcofarid@gmail.com>
2020-07-27 22:18:45 -0500

    Update ServicioDocumentacionApplication.java

--- src/main/java/co/gov/icfes/ServicioDocumentacionApplication.java
+++ src/main/java/co/gov/icfes/ServicioDocumentacionApplication.java
@@ -3,12 +3,13 @@ package co.gov.icfes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
 @EnableScheduling
+@EnableEurekaClient
 public class ServicioDocumentacionApplication {
 
 	public static void main(String[] args) {
